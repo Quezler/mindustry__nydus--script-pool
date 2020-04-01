4d40b20875d1ea62832cc580130b107fd8109d57
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-01 15:32:26 +0200

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,5 @@
 // toggle sandbox mode
 Vars.state.rules.infiniteResources = !Vars.state.rules.infiniteResources
 Call.onSetRules(Vars.state.rules)
-// last object is returned to the chat?
-"sandbox mode is now " + Vars.state.rules.infiniteResources
+// last object is returned to the chat
+"sandbox mode is now [accent]" + Vars.state.rules.infiniteResources + "[]"
