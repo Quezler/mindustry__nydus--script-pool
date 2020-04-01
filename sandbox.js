6aea9e37178d6c02cc5c9598170a66451b656de7
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-01 15:21:26 +0200

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
 // toggle sandbox mode
-Vars.state.rules.infiniteResources != Vars.state.rules.infiniteResources
+Vars.state.rules.infiniteResources = !Vars.state.rules.infiniteResources
 Call.onSetRules(Vars.state.rules)
+return "sandbox mode is now " + Vars.state.rules.infiniteResources
