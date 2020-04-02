bf8bb06aac1736252aa7fdc745f53208f7e436a3
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-02 14:07:27 +0200

    Update place.js

--- place.js
+++ place.js
@@ -1,3 +1,4 @@
 // "/ts place block=Blocks.coreShard"
 me().tileOn().setNet(block, me().team, 0);
 block.placed(me().tileOn());
+"placed a [accent]"+ block.name() +"[] for team [#"+ me().team.color +"]"+ me().team +"[]"
