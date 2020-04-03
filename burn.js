22888997108e253e354083c1414cbcb08742b5a4
bad-egg365 <63074869+bad-egg365@users.noreply.github.com>
2020-04-03 19:19:24 +0300

    Update burn.js

--- burn.js
+++ burn.js
@@ -1,3 +1,3 @@
 //Sets everething on fire
 Vars.tileGroup.all().each(cons((e) => Fire.create(e.tile) ))
-"Some people just want to watch the world burn"
+"Some people just want to watch the world burn."
