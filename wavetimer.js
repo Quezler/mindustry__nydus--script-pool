56dbeb84c8074a8cc6d968d65ad13a5990622955
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-03 10:30:04 +0200

    Update wavetimer.js

--- wavetimer.js
+++ wavetimer.js
@@ -1,4 +1,4 @@
 // "/ts wavetimer seconds=5"
-Vars.state.rules.waveSpacing = seconds / 60;
+Vars.state.rules.waveSpacing = seconds * 60;
 Call.onSetRules(Vars.state.rules);
 Strings.format("wave spacing will be [accent]{0}[] second(s) after the next wave.", seconds);
