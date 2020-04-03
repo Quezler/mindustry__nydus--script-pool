ed1272e8959273cee534ecc7414838e6adcf1571
bad-egg365 <63074869+bad-egg365@users.noreply.github.com>
2020-04-03 23:36:27 +0300

    Update smite.js

--- smite.js
+++ smite.js
@@ -1,3 +1,4 @@
 //Kills the selected player
 // /ts smite player="name" 
-Vars.playerGroup.find(boolf(p => p.name.match(player))).kill()
+Vars.playerGroup.find(boolf(p => p.name.match(player))).kill();
+"Target smited."
