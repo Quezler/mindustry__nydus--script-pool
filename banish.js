8ed58cdb67e2bb030567f86c5a544fc59f12e9d7
bad-egg365 <63074869+bad-egg365@users.noreply.github.com>
2020-04-03 22:43:04 +0300

    Update banish.js

--- banish.js
+++ banish.js
@@ -1,4 +1,4 @@
 // /ts player="name",server="server",port=6567
 // If port is left empty default port is used
-((typeof port === 'undefined') ? 6567 : port)
-Call.onConnect(Vars.playerGroup.find(boolf(p => p.name.match(player))).con, server, port)
+((typeof port === 'undefined') ? 6567 : port);
+Call.onConnect(Vars.playerGroup.find(boolf(p => p.name.match(player))).con, server, port);
