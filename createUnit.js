bb69c6485df3f52cd447ba7250d90101ae8d3229
testing_online <>
2020-04-04 12:01:51 +0200

    Ad optional team to createUnit + comments

--- createUnit.js
+++ createUnit.js
@@ -1,19 +1,26 @@
-// /ts createUnit unit=UnitTypes.dagger; amount=10 (optional)
+// /ts createUnit unit=UnitTypes.dagger; amount=10 (optional); team=Team.crux (optional, defaults to player's team)
 if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "createUnit";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
   const state = ts[ts.currentScriptName];
 
   const player = me();
+
+  // set default if optional parameters are not specified
   const amount1 = (typeof amount === 'undefined') ? 1 : amount;
+  const team1 = (typeof team === 'undefined') ? player.team : team;
+
+  // create the units
   for(i=0;i<amount1;i++){
-    var e = unit.create(player.team);
+    var e = unit.create(team1);
     e.set(player.x, player.y);
     e.add();
   }
-  // clean the arguments
+
+  // clean the arguments to avoid accidentally spawning 1000 crux erads
   delete unit;
   delete amount;
+  delete team;
 
 };
 ts[ts.currentScriptName].function();
