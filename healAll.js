52ff8a2d7423f04316ba08f2298bae0a0930b532
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-04 15:01:26 +0500

    add team optional arg

--- healAll.js
+++ healAll.js
@@ -1,18 +1,21 @@
 // Heals all allied players
+// ts healAll team=Team.<name [optional]> 
 if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "healAll";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
     const state = ts[ts.currentScriptName];
+    
+    if (team === undefined) team = me().team
   
     playerGroup = Vars.playerGroup.all();
     
     for (i = 0; i < playerGroup.size; i++) {
         p = playerGroup.get(i);
-        if (p.team === me().team) {
+        if (p.team === team) {
             p.heal();
         }
     }
     
-    "[green]Healed all[] " + String(me().team) + " [green]players"
+    "[green]Healed all[] " + String(team) + " [green]players"
 };
 ts[ts.currentScriptName].function();
