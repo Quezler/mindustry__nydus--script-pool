a2eb3de00af142820ff5e8fd02b314c18f773dbd
bad_egg <63074869+bad-egg365@users.noreply.github.com>
2020-04-04 16:36:06 +0300

    Some fun things

---
+++ bounce.js
@@ -0,0 +1,4 @@
+//Changes your mech into a javelin and applies the overdrive effect (Have fun! :D)
+me().mech = Mechs.javelin
+me().applyEffect(StatusEffects.overdrive,10000)
+"Have fun!"
---
+++ overdrive.js
@@ -0,0 +1,3 @@
+//Applies the overdrive effect to you(cuz i'm lazy)
+me().applyEffect(StatusEffects.overdrive,10000)
+"Effect applied."
