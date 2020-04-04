51c5debc34630dc1c1165400c2418290f19644ba
bad-egg365 <63074869+bad-egg365@users.noreply.github.com>
2020-04-04 14:30:29 +0300

    Update place.js

--- place.js
+++ place.js
@@ -1,6 +1,6 @@
 // "/ts place block=Blocks.coreShard, team=Team.<team>"
 
-team = (typeof team == undefined) ? me().team : team;
+((typeof team === 'undefined') ? me().team : team);
 
 me().tileOn().setNet(block, team, 0);
 block.placed(me().tileOn());
