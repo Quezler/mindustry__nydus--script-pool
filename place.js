365343fa6d12e0cf8ecbcba84016b9cdc023f8d7
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-04 16:17:10 +0500

    fix?

--- place.js
+++ place.js
@@ -1,9 +1,7 @@
 // "/ts place block=Blocks.coreShard, team=Team.<team>"
 
 //team = (typeof team == undefined) ? me().team : team;
-if (team == undefined) {
-    team = me().team
-}
+team = me().team
 
 me().tileOn().setNet(block, team, 0);
 block.placed(me().tileOn());
