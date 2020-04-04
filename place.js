111eb417374e723c9f397f0d78235eb10ffc5f53
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-04 16:07:31 +0500

    fixing issues

--- place.js
+++ place.js
@@ -2,11 +2,9 @@
 
 team = (typeof team === undefined) ? me().team : team;
 
-String(team);
-
 me().tileOn().setNet(block, team, 0);
 block.placed(me().tileOn());
 
 "placed a [accent]"+ block.name +"[] for team [#"+ team.color +"]"+ team +"[]";
 
-team = undefined;
+delete team;
