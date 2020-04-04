35be6cea1b4bdd85d4eb1f593d07ca954e5d7f54
testing_online <>
2020-04-04 11:30:19 +0200

    Add amount to createunit

--- createUnit.js
+++ createUnit.js
@@ -1,15 +1,19 @@
-// /ts createUnit unit=UnitTypes.dagger
+// /ts createUnit unit=UnitTypes.dagger; amount=10 (optional)
 if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "createUnit";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
   const state = ts[ts.currentScriptName];
 
   var player = me();
-  var e = unit.create(player.team);
-  e.set(player.x, player.y);
-  e.add();
+  var amount = (typeof amount === 'undefined') ? 1 : amount;
+  for(i=0;i<amount;i++){
+    var e = unit.create(player.team);
+    e.set(player.x, player.y);
+    e.add();
+  }
   // clean the arguments
   delete unit;
+  delete amount;
 
 };
 ts[ts.currentScriptName].function();
