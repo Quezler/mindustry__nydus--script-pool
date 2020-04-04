3d0d8e7f0d30c7ea73e6af4d343124a7ae15de4a
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-04 16:58:11 +0500

    fix for it returing just true

--- place.js
+++ place.js
@@ -5,6 +5,6 @@ team = ((typeof team === 'undefined') ? me().team : team);
 me().tileOn().setNet(block, team, 0);
 block.placed(me().tileOn());
 
-"placed a [accent]"+ String(block.name) +"[] for team [#"+ String(team.color) +"]"+ String(team) +"[]"
+me().sendMessage("placed a [accent]"+ String(block.name) +"[] for team [#"+ String(team.color) +"]"+ String(team) +"[]");
 
-team = undefined
+delete team
