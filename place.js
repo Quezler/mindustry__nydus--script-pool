55b636bf6f886828768ad7b3fe7df7af4fdcb85d
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-04 15:54:56 +0500

    Update place.js

--- place.js
+++ place.js
@@ -1,10 +1,10 @@
 // "/ts place block=Blocks.coreShard, team=Team.<team>"
 
-team = (typeof team === undefined) ? me().team : team
+team = (typeof team === undefined) ? me().team : team;
 
 me().tileOn().setNet(block, team, 0);
 block.placed(me().tileOn());
 
-"placed a [accent]"+ block.name +"[] for team [#"+ team.color +"]"+ team +"[]"
+"placed a [accent]"+ block.name +"[] for team [#"+ team.color +"]"+ team +"[]";
 
-team = undefined
+team = undefined;
