7a830fc5ccf347af7c6eff4523cee646038643f7
bad_egg <63074869+bad-egg365@users.noreply.github.com>
2020-04-06 15:06:59 +0300

    Fix

--- name.js
+++ name.js
@@ -1,18 +1,18 @@
 //changes the name of the targeted player(defaults to you)
-// "/ts name p="player" n="new name" "
+// "/ts name pl="player" n="new name" "
 if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "name";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
   const state = ts[ts.currentScriptName];
 
  if ((typeof p)==='undefined') {
- p=me().name;    
+ pl=me().name;    
  }
  if ((typeof p)==='object') {
- p=me().name    
+ pl=me().name    
  }
- Vars.playerGroup.find(boolf(p => p.name.match(p))).name=n;
- delete p;
+ Vars.playerGroup.find(boolf(p => p.name.match(pl))).name=n;
+ delete pl;
  delete n;
  me().sendMessage("Name changed.")
   
