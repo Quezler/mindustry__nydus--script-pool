41a6c31fdbfe542956d92908df42d9c3ab467afb
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-13 17:31:36 +0500

    fix

--- banish.js
+++ banish.js
@@ -1,20 +1,9 @@
 // /ts player="name",server="server",port=6567
 // If port is left empty default port is used
 
-port = 6567
-player = undefined
-server = undefined
-
-for (i = 0; i < args.length; i++) {
-	if (typeof args[i] === 'string') {
-		if (typeof player === 'undefined') {
-			p = Vars.playerGroup.find(boolf(p => p.name.match(args[i])));
-			player = p == null ? undefined : p;
-		} else {
-			server = args[i]
-		}
-	} else if (Number.isInteger(args[i])) port = args[i]
-}
+port = typeof args[2] === 'undefined' ? 6567 : args[2]
+player = args[0]
+server = args[1]
 
 if (typeof player === 'undefined') {
 	Vars.scripter.sendMessage("Either the player was not found or the name was wrong")
