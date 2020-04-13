025c79152fa45c4bd272c4471a6ab1bf79e698fa
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-13 17:15:03 +0500

    fix

--- wavetimer.js
+++ wavetimer.js
@@ -1,4 +1,4 @@
 // "/ts wavetimer seconds=5"
 Vars.state.rules.waveSpacing = args[0] * 60;
 Call.onSetRules(Vars.state.rules);
-Strings.format("wave spacing will be [accent]{0}[] second(s) after the next wave.", seconds);
+Strings.format("wave spacing will be [accent]{0}[] second(s) after the next wave.", args[0]);
