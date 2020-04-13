69a04e15b2d9c84ba73a03ebd630d8ebd2e58b97
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-13 17:10:06 +0500

    fix

--- slagCircle.js
+++ slagCircle.js
@@ -1,8 +1,8 @@
 // /ts slagCircle player=scripter
 
-player = args.length > 0 ? args[0] : Vars.scripter;
+player = args.length > 0 ?  Vars.playerGroup.find(boolf(p => p.name.match(args[0]))) : Vars.scripter;
 
 for(i=0;i<360;i++)
 	for(j=0;j<0.5;j+=0.08)
-		Calls.createBullet(Bullets.slagShot, Team.sharded, player.x, player.y, i, j, 30);
+		Calls.createBullet(Bullets.slagShot, player.team, player.x, player.y, i, j, 30);
 0;
