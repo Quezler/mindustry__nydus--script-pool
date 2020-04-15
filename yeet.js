f57867292a6d7c3b5f6913ec27da4c331dad9a3c
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-15 16:16:26 +0500

    sigh

--- yeet.js
+++ yeet.js
@@ -48,7 +48,7 @@ ts[ts.currentScriptName].function = function(){
 
     if (String(player) == 'null') Vars.scripter.sendMessage(String(p) + "[scarlet] was not found")
     else {
-        //Call.onConnect(player.con, server[0], server[1]);
+        Call.onConnect(player.con, server[0], server[1]);
         Vars.scripter.sendMessage(player.name + "[#E8DAEF] got yeeted to [#A9DFBF]" + server[0] + "[]:[#F9E79F]" + server[1]);
     }
 
