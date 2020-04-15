144ab4d42e7cff3939943c3690ecebe6698a4f4e
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-04-15 16:08:36 +0500

    updated player finder

--- popup.js
+++ popup.js
@@ -5,32 +5,74 @@ if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {
 ts[ts.currentScriptName].function = function(){
     const state = ts[ts.currentScriptName];
 
-    tryFindPlayer = function(name) {
-        stripColor = function(name) {
+    function tryFindPlayer(name) {
+        function stripColor(colored) {
+            var colors = [
+                "clear", "black", "white", "lightgray",
+                "gray", "darkgray", "blue", "navy",
+                "royal", "slate", "sky", "cyan",
+                "teal", "green", "acid", "lime",
+                "forest", "olive", "yellow", "gold",
+                "goldenrod", "orange", "brown", "tan",
+                "brick", "red", "scarlet", "coral", "salmon",
+                "pink", "magenta", "purple", "violet", "maroon"
+            ];
+    
             var stripped = ""
+            var color = ""
             var inColor = false
-            for (i = 0; i < name.length; i++) {
-                if (name[i] == "[") {
+    
+            for (i = 0; i < colored.length; i++) {
+                if (colored[i] == "[") {
                     inColor = true;
-                } else if (name[i] == "]") {
+                    continue;
+                } else if (colored[i] == "]") {
                     inColor = false;
+                    if(typeof colors.find(c => c == color) == 'undefined') {
+                        if (!color.match("(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{8}$)")) {
+                            stripped += "[" + color + "]";
+                        }
+                    }
+                    color = "";
                     continue;
                 }
-                if (inColor) continue;
-                
-                stripped += name[i];
+                if (inColor) {
+                    if (i == colored.length - 1) {
+                        stripped += "["
+                        stripped += color;
+                        stripped += colored[i];
+                        break;
+                    }
+                    color += colored[i];
+                    continue;
+                }
+                stripped += colored[i];
             }
             return stripped;
         }
-
-        player = Vars.playerGroup.all().find(boolf(p => p.name.match(name)));
-        if (String(player) == 'null') {
-            player = Vars.playerGroup.all().find(boolf(p => stripColor(p.name).match(stripColor(args[0]))))
-            if (String(player) == 'null') {
-                player = Vars.playerGroup.all().find(boolf(p => p.name == name))
+    
+        function escapeBracket(unescaped) {
+            var escaped = "";
+            for(i = 0; i < unescaped.length; i++) {
+                if (unescaped[i] == "[") {
+                    escaped += "\\[";
+                    continue;
+                }
+                escaped += unescaped[i];
+            }
+            return escaped;
+        }
+        
+        player = Vars.playerGroup.all().find(boolf(p => stripColor(p.name).match(stripColor(name))))
+        if (player == 'null') {
+            player = Vars.playerGroup.all().find(boolf(p => p.name.match(escapeBracket(name))));
+            if (player == 'null') {
+                player = Vars.playerGroup.all().find(boolf(p => stripColor(p.name).match(escapeBracket(stripColor(name)))));
+                if (player == 'null') {
+                    player = Vars.playerGroup.all().find(boolf(p => p.name == name));
+                }
             }
         }
-
         return player;
     }
 
