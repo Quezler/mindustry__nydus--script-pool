aa87fff44524e5aa02ebc7e59d7cc18ce83e1b5e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-17 15:19:28 +0200

    Update and rename bp.js to oily.js

---
+++ oily.js
@@ -0,0 +1,6 @@
+for(var x = 0; x < world.width(); x++){
+    for(var y = 0; y < world.height(); y++){
+        var t = world.tile(x, y);
+        if(t.block == Blocks.air) t.setFloor(Blocks.tar);
+    }
+}
