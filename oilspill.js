5692166c91a69a1bc057c4ffc9ae2447499591c7
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-17 15:20:19 +0200

    Update and rename gulf.js to oilspill.js

--- oilspill.js
+++ oilspill.js
@@ -1,5 +1,5 @@
-for(var x = 0; x < world.width(); x++){
-    for(var y = 0; y < world.height(); y++){
+for(var x = 0; x < Vars.world.width(); x++){
+    for(var y = 0; y < Vars.world.height(); y++){
         var t = Vars.world.tile(x, y);
         if(t.block == Blocks.air) t.setFloor(Blocks.tar);
     }
