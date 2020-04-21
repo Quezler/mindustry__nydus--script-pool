db766375e8c55523f3b1a77da35e37c3bdf9c509
braainsiogame <deadisdead69@gmail.com>
2020-04-21 23:24:57 +0700

    added factoryChanger.js

---
+++ factoryChanger.js
@@ -0,0 +1,18 @@
+ if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "factoryChanger";
+if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
+ts[ts.currentScriptName].function = function(){
+  const state = ts[ts.currentScriptName];
+
+  Vars.scripter.sendMessage("[red] Warning: This ts may cause lag to the server.")
+var factory = args[0]
+var mech = args[1]
+var spawn = args[2]
+factory.unitType = mech
+if(!(spawn == null)){
+  Blocks.factory.maxSpawn = spawn;
+}
+
+0;
+
+};
+ts[ts.currentScriptName].function();
