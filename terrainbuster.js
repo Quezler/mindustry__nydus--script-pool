15941088632ae6f824ddf6985e2460d4d0ee3a80
braainsiogame <deadisdead69@gmail.com>
2020-04-23 11:36:06 +0700

    fixed terrainbuster.js

--- terrainbuster.js
+++ terrainbuster.js
@@ -1,6 +1,6 @@
 for(var x = 0; x < Vars.world.width(); x++){
     for(var y = 0; y < Vars.world.height(); y++){
         var t = Vars.world.tile(x, y);
-        if(t.block() == Blocks.rocks, Blocks.sporerocks, Blocks.icerocks, Blocks.cliffs, Blocks.sporePine, Blocks.snowPine, Blocks.pine, Blocks.shrubs, Blocks.whiteTree, Blocks.whiteTreeDead, Blocks.sporeCluster,iceSnow) t.setBlock(Blocks.air);
+        if(t.block() == Blocks.rocks, Blocks.sporerocks, Blocks.icerocks, Blocks.cliffs, Blocks.sporePine, Blocks.snowPine, Blocks.pine, Blocks.shrubs, Blocks.whiteTree, Blocks.whiteTreeDead, Blocks.sporeCluster,) t.setBlock(Blocks.air);
     }
 }
