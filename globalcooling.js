f8864ad1d9163444db309b1db5981e0af361a643
braainsiogame <deadisdead69@gmail.com>
2020-04-23 10:47:31 +0700

    ice ice ice ice ice

---
+++ globalcooling.js
@@ -0,0 +1,6 @@
+for(var x = 0; x < Vars.world.width(); x++){
+    for(var y = 0; y < Vars.world.height(); y++){
+        var t = Vars.world.tile(x, y);
+        if(t.block() == Blocks.air) t.setFloor(Blocks.ice);
+    }
+}
