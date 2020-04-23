59faee422cc794504c2423c02186151e0a5124dd
braainsiogame <deadisdead69@gmail.com>
2020-04-23 11:32:41 +0700

    sporerocks is not defined

--- terrainbuster.js
+++ terrainbuster.js
@@ -1,6 +1,6 @@
 for(var x = 0; x < Vars.world.width(); x++){
     for(var y = 0; y < Vars.world.height(); y++){
         var t = Vars.world.tile(x, y);
-        if(t.block() == Blocks.rocks, sporerocks, icerocks, cliffs, sporePine, snowPine, pine, shrubs, whiteTree, whiteTreeDead, sporeCluster,iceSnow) t.setBlock(Blocks.air);
+        if(t.block() == Blocks.rocks, Blocks.sporerocks, Blocks.icerocks, Blocks.cliffs, Blocks.sporePine, Blocks.snowPine, Blocks.pine, Blocks.shrubs, Blocks.whiteTree, Blocks.whiteTreeDead, Blocks.sporeCluster,iceSnow) t.setBlock(Blocks.air);
     }
 }
