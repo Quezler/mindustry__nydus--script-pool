25401428f457007a4ffa06fc1fbf305d630286ae
Photemy <64468573+Photemy@users.noreply.github.com>
2020-04-28 17:32:23 +0200

    Update triGun.js

--- triGun.js
+++ triGun.js
@@ -4,7 +4,7 @@
 // /ts laserGun <player> <bullet>
 // /ts laserGun me Bullets.meltdownLaser
 // Sorry for stealing code from laserGun, I hope you dont mind. -Photemy
-if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "laserGun";
+if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "triGun";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
   const state = ts[ts.currentScriptName];
@@ -15,21 +15,21 @@ ts[ts.currentScriptName].function = function(){
     Vars.scripter.sendMessage("Trigun has been disabled");
   }else{
     const bullet   = args[0] || Bullets.missileJavelin;
-    const player   = args[6] || Vars.scripter;
+    const p        = args[6] || Vars.scripter;
     const accuracy = args[1] || 10;
-    const team     = args[3] || player.team;
+    const team     = args[3] || p.team;
     const rotspd   = args[4] || 1;
     const reload   = args[5] || 1;
     var   mov      = 0
 
       state.task = new java.util.TimerTask() {run(){
-         if(player.isShooting()){
+         if(p.isShooting()){
             for(j=0;j<360;j=j+120){
                Calls.createBullet(
                   Bullets.cryoShot,
                   team,
-                  (player.x+(Math.cos((j+rot)/57.3))*5*mov),
-                  (player.y+(Math.sin((j+rot)/57.3))*5*mov),
+                  (p.x+(Math.cos((j+rot)/57.3))*5*mov),
+                  (p.y+(Math.sin((j+rot)/57.3))*5*mov),
                   j,
                   0,
                   0.2
@@ -43,9 +43,9 @@ ts[ts.currentScriptName].function = function(){
                   Calls.createBullet(
                      bullet,
                      team,
-                     (player.x+Math.cos((i+rot)/57.3)*50),
-                     (player.y+Math.sin((i+rot)/57.3)*50),
-                     player.rotation+(Math.random()*accuracy)-(accuracy/2),
+                     (p.x+Math.cos((i+rot)/57.3)*50),
+                     (p.y+Math.sin((i+rot)/57.3)*50),
+                     p.rotation+(Math.random()*accuracy)-(accuracy/2),
                      1,
                      1
                   );
@@ -56,9 +56,9 @@ ts[ts.currentScriptName].function = function(){
                Calls.createBullet(
                   Bullets.slagShot,
                   team,
-                  (player.x+(Math.cos((f+rot)/57.3))*5*mov),
-                  (player.y+(Math.sin((f+rot)/57.3))*5*mov),
-                  j,
+                  (p.x+(Math.cos((f+rot)/57.3))*5*mov),
+                  (p.y+(Math.sin((f+rot)/57.3))*5*mov),
+                  0,
                   0,
                   0.2
                  );
@@ -68,10 +68,10 @@ ts[ts.currentScriptName].function = function(){
             }
          };
     }};
-    state.timer = new java.util.Timer("laserGun")
+    state.timer = new java.util.Timer("triGun")
     state.timer.schedule(state.task, 0,100);
     state.running = true;
-    Vars.scripter.sendMessage(" " + player.name + " is now using Trigun. Arguments in order: bullet, accuracy, team, rotspd, reload, player.");
+    Vars.scripter.sendMessage(" " + p.name + " is now using Trigun. Arguments in order: bullet, accuracy, team, rotspd, reload, player.");
   }
 };
 ts[ts.currentScriptName].function();0;
