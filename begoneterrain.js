a09cdcf4503c5d360236a60f78e334ef7865add4
braainsiogame <deadisdead69@gmail.com>
2020-04-30 19:05:21 +0700

    sweep sweep

--- begoneterrain.js
+++
@@ -1,6 +0,0 @@
-for(var x = 0; x < Vars.world.width(); x++){
-    for(var y = 0; y < Vars.world.height(); y++){
-        var t = Vars.world.tile(x, y);
-        if(t.block() == Blocks.rocks) t.setFloor(Blocks.air);
-    }
-}
