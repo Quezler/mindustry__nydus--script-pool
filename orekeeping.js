63e6e2f55e06043fe5b8316f8d442fb2ba69b903
braainsiogame <deadisdead69@gmail.com>
2020-05-01 16:20:04 +0700

    thank you blockzilla

--- orekeeping.js
+++ orekeeping.js
@@ -1,9 +1,9 @@
 for(var x = 0; x < Vars.world.width(); x++){
     for(var y = 0; y < Vars.world.height(); y++){
         var t = Vars.world.tile(x, y);
-        if(t.block() == Blocks.oreCopper || t.block() ==  Blocks.oreLead || 
-            t.block() == Blocks.oreScrap || t.block() == Blocks.oreTitanium || 
-            t.block() == Blocks.oreThorium) {
+        if(t.floor() == Blocks.oreCopper || t.floor() ==  Blocks.oreLead || 
+            t.floor() == Blocks.oreScrap || t.floor() == Blocks.oreTitanium || 
+            t.floor() == Blocks.oreThorium) {
             
             t.setBlock(Blocks.air);
         }
