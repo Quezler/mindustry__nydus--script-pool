65420bbef670f465265203044a621be18b5d790e
braainsiogame <deadisdead69@gmail.com>
2020-05-01 16:58:53 +0700

    on ho

--- orebuster.js
+++ orebuster.js
@@ -1,29 +1,23 @@
-// i copied orekeeping old code don't @ me
 count = 0;
 for(var x = 0; x < Vars.world.width(); x++){
     for(var y = 0; y < Vars.world.height(); y++){
         var t = Vars.world.tile(x, y);
-        if(t.floor() == Blocks.oreCopper || t.floor() ==  Blocks.oreLead || 
-            t.floor() == Blocks.oreScrap || t.floor() == Blocks.oreTitanium || 
-            t.floor() == Blocks.oreThorium) {
         if(t.overlay() == Blocks.oreCopper || t.overlay() ==  Blocks.oreLead || 
             t.overlay() == Blocks.oreScrap || t.overlay() == Blocks.oreTitanium || 
             t.overlay() == Blocks.oreThorium || t.overlay() == Blocks.oreCoal) {
-
-            t.setBlock(Blocks.air);
+            
             t.setOverlay(Blocks.air);
             count++;
         }
     }
 }
 
-0; 
 function sync(player) {
     function sendWorldData(p) {
         if (typeof ByteArrayOutputStream == 'undefined') importPackage(java.io);
         if (typeof FastDeflaterOutputStream == 'undefined') importPackage(Packages.arc.util.io)
         if (typeof NetworkIO == 'undefined') importPackage(Packages.mindustry.net)
-
+        
         stream = new ByteArrayOutputStream();
         def = new FastDeflaterOutputStream(stream);
         NetworkIO.writeWorld(p, def);
