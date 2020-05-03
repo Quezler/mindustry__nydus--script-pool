0a69c6fdec67059091a6bb9671258c11109966c6
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-03 13:11:11 +0200

    Update and rename gameoflife.js to gameofdoor.js

--- gameofdoor.js
+++ gameofdoor.js
@@ -4,3 +4,9 @@ for(var x = 0; x < Vars.world.width(); x++){
         if(t.block() == Blocks.air) t.setNet(Blocks.door, Vars.scripter.team, 0);
     }
 }
+Vars.state.rules.spawns.clear();
+Vars.state.rules.waveSpacing = 60;
+Vars.state.rules.waves = true;
+Vars.state.rules.waitForWaveToEnd = false;
+Vars.logic.runWave();
+Call.onSetRules(Vars.state.rules);
