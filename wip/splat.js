3a684f3cd2aebb90f0b228be37188afdb2351df5
testing_online <>
2020-05-15 14:03:13 +0200

    Wall effects

--- wip/splat.js
+++ wip/splat.js
@@ -7,22 +7,81 @@ ts[ts.currentScriptName].function = function(){
     Vars.scripter.sendMessage("[yellow] no action specified. use /ts splat 'start'|'stop'|'status'");
   } else switch(args[0]) {
     case "start":
-      // Start the wall placer timer
-      try{state.placer.cancel()}catch(e){} // just in case
+      try{state.timer.cancel()}catch(e){} // just in case
       const task = new java.util.TimerTask() { run() {
         Vars.playerGroup.all().each(cons(function(p) {
-          if(p.tileOn().block() === Blocks.air) {
-            p.tileOn().setNet(Blocks.copperWall, p.team, 0);
-            Blocks.copperWall.placed(p.tileOn());
+          const tileOn = p.tileOn();
+          const blockOn = tileOn.block();
+          if(blockOn === Blocks.air) {
+            tileOn.setNet(Blocks.copperWall, p.team, 0);
+            Blocks.copperWall.placed(tileOn);
+          }else if(tileOn.getTeam() !== p.team) switch(blockOn){
+            //TODO tweak status effects and damages
+            case Blocks.copperWall:
+            case Blocks.copperWallLarge:
+              p.applyEffect(StatusEffects.burning,2)
+              p.damage(10);
+              break;
+            case Blocks.titaniumWall:
+            case Blocks.titaniumWallLarge:
+              p.applyEffect(StatusEffects.freezing,1)
+              p.damage(20);
+              break;
+            case Blocks.thoriumWall:
+            case Blocks.thoriumWallLarge:
+              p.applyEffect(StatusEffects.corroded,5)
+              p.damage(50);
+              break;
+            case Blocks.plastaniumWall:
+            case Blocks.plastaniumWallLarge:
+              p.applyEffect(StatusEffects.overdrive,5)
+              p.damage(70);
+              break;
+            case Blocks.phaseWall:
+            case Blocks.phaseWallLarge:
+              p.applyEffect(StatusEffects.tarred,10)
+              p.damage(30);
+              break;
+            case Blocks.surgeWall:
+            case Blocks.surgeWallLarge:
+              p.applyEffect(StatusEffects.shocked,7)
+              p.damage(100);
+              break;
+          }else if(tileOn.getTeam() == p.team) switch(blockOn){
+            //TODO tweak buffs
+            case Blocks.copperWall:
+            case Blocks.copperWallLarge:
+              p.health+=10;
+              break;
+            case Blocks.titaniumWall:
+            case Blocks.titaniumWallLarge:
+              p.health+=10;
+              break;
+            case Blocks.thoriumWall:
+            case Blocks.thoriumWallLarge:
+              p.health+=10;
+              break;
+            case Blocks.plastaniumWall:
+            case Blocks.plastaniumWallLarge:
+              p.health+=10;
+              break;
+            case Blocks.phaseWall:
+            case Blocks.phaseWallLarge:
+              p.health+=10;
+              break;
+            case Blocks.surgeWall:
+            case Blocks.surgeWallLarge:
+              p.health+=10;
+              break;
           }
         }));
       }};
-      state.placer = new java.util.Timer("splat placer")
-      state.placer.schedule(task, 0,100);
+      state.timer = new java.util.Timer("splat")
+      state.timer.schedule(task, 0,100);
       state.running = true;
       break;
     case "stop":
-      state.placer.cancel();
+      state.timer.cancel();
       state.running = false;
       break;
     case "status":
