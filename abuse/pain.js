73f36ab3379e6446918d4cb3031471dcdabf3393
braainsiogame <deadisdead69@gmail.com>
2020-05-25 13:45:49 +0700

    pain

---
+++ abuse/pain.js
@@ -0,0 +1,10 @@
+// Remember to change "yourScriptName"
+// (just the string. the rest is adjusted automatically)
+// with the name of your script:
+if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "pain";
+if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
+ts[ts.currentScriptName].function = function(){
+  const state = ts[ts.currentScriptName];
+Blocks.conveyor.speed = 0.001, Blocks.titaniumConveyor.speed = 0.001, Blocks.armoredConveyor.speed = 0.001, UnitTypes.dagger.weapon.reload = 0, Mechs.dart.speed = 0.0001, Call.onInfoToast([red] you have entered the admin abuse zone,300;
+};
+ts[ts.currentScriptName].function();
