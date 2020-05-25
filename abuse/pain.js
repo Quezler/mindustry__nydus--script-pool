c226747bd2199827a91191993691e2daa8560c69
braainsiogame <deadisdead69@gmail.com>
2020-05-25 13:56:51 +0700

    this is stupid

--- abuse/pain.js
+++
@@ -1,2 +0,0 @@
-
-Blocks.conveyor.speed = 0.001, Blocks.titaniumConveyor.speed = 0.001, Blocks.armoredConveyor.speed = 0.001, UnitTypes.dagger.weapon.reload = 0, Mechs.dart.speed = 0.0001, Call.onInfoToast([red] you have entered the admin abuse zone,300);
