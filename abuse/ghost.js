d055f922bc1e8d52c0e4092b515df52061d4414d
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-07-11 10:01:48 +0500

    cleanup

--- abuse/ghost.js
+++ abuse/ghost.js
@@ -1,5 +1,6 @@
-// /ts ghosting new_name?
-// Toggles all player names to be the specified one or an empty string
+// Usage:
+//   /ts ghosting new_name?
+//   Changes all player names to an empty string, or optionally to the specified one
 
 if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "yourScriptName";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
@@ -19,22 +20,32 @@ ts[ts.currentScriptName].function = function(){
 
     if (!state.ghosted) {
         playerGroup = Vars.playerGroup.all();
+
         for(i = 0; i < playerGroup.size; i++) {
             state.originalNames[playerGroup.get(i).uuid] = playerGroup.get(i).name;
             playerGroup.get(i).name = newName;
+
         }
-        Vars.scripter.sendMessage("Ghosted")
+
+        Vars.scripter.sendMessage("[#D7BDE2]G[#AED6F1]h[#D7BDE2]o[#AED6F1]s[#D7BDE2]t[#AED6F1]e[#D7BDE2]d")
     } else {
+
         playerGroup = Vars.playerGroup.all();
+
         for(i = 0; i < playerGroup.size; i++) {
-            uuid = Object.keys(state.originalNames).find(uuid => playerGroup.get(i).uuid == uuid)
+            uuid = Object.keys(state.originalNames).find(uuid => playerGroup.get(i).uuid == uuid);
+
             if (typeof uuid != 'undefined') {
                 playerGroup.get(i).name = state.originalNames[uuid];
             }
+
         }
         state.originalNames = {};
-        Vars.scripter.sendMessage("UnGhosted")
+
+        Vars.scripter.sendMessage("[#F7DC6F]U[#FFAB4C]n[#F7DC6F]G[#FFAB4C]h[#F7DC6F]o[#FFAB4C]s[#F7DC6F]t[#FFAB4C]e[#F7DC6F]d")
     }
+
+    delete newName;
 };
 ts[ts.currentScriptName].function();
 0;
