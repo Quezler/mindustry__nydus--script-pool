66a7477389c55aca1751758c99f27fd5eecfa636
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2020-08-22 18:40:06 +0500

    .

--- rule/gamemode.js
+++ rule/gamemode.js
@@ -2,20 +2,43 @@ if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "gamemode";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
     const state = ts[ts.currentScriptName];
-//how
-		var gamemode = args[0]
-		var hardmode = Vars.state.rules.waveSpacing / 2 //and i messed up agaain.
-       if (gamemode === "attack") {
-         js Vars.state.rules.attackMode = true; Vars.scripter.sendMessage("Enabled attack mode.")
-        }
-		if (gamemode === "survival") {
-            js Vars.state.rules.attackMode = false; Vars.state.rules.waveTimer = true; Vars.state.rules.pvp = false; Vars.scripter.sendMessage
-        }
-		
-	   if (gamemode === "hardmode") {
-		   js Vars.state.rules.waveSpacing = hardmode; Vars.scripter.sendMessage("enabled hardmode, have fun.");
-        }
-	   if (gamemode === "sandbox")
-		 js Vars.state.rules.infiniteResources = true; Vars.state.rules.waveTimer = false; Vars.scripter.sendMessage("Enabled sandbox mode");
-	    }
+    //how
+    
+    if (args.length === 0) {
+        Vars.scripter.sendMessage("Provide a gamemode to change to");
+        return;
+    }
+    
+    var gamemode = args[0];
+    var hardmode = Vars.state.rules.waveSpacing / 2 //and i messed up agaain.
+    
+    if (gamemode === "attack") {
+        Vars.state.rules.attackMode = true; 
+        Vars.state.rules.waveTimer = false; 
+        Vars.state.rules.pvp = false; 
+        Vars.scripter.sendMessage("Gamemode set to attack");
+      
+    } else if (gamemode === "survival") {
+        Vars.state.rules.attackMode = false; 
+        Vars.state.rules.waveTimer = true; 
+        Vars.state.rules.pvp = false; 
+        Vars.scripter.sendMessage("Gamemode set to survival");
+        
+    } else if (gamemode === "hardmode") {
+       js Vars.state.rules.waveSpacing = hardmode;
+       Vars.scripter.sendMessage("Gamemode set to hardmode");
+       
+    } else if (gamemode === "sandbox") {
+        Vars.state.rules.attackMode = false; 
+        Vars.state.rules.waveTimer = false; 
+        Vars.state.rules.pvp = false;
+        Vars.scripter.sendMessage("Gamemode set to sandbox");
+        
+    } else {
+        Vars.scripter.sendMessage("'" + gamemode + "' is not a valid gamemode");
+    }
+    
+    Call.onSetRules(Vars.state.rules);
+}
 ts[ts.currentScriptName].function();
+0;
