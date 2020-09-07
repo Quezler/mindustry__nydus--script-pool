ee5034d991bc2eba35277947c89428fe52c84980
braainsiogame <deadisdead69@gmail.com>
2020-09-07 21:45:57 +0700

    bap

--- player/admin.js
+++ player/admin.js
@@ -4,7 +4,7 @@ if(typeof ts === 'undefined') ts = {}; ts.currentScriptName = "admin";
 if(typeof ts[ts.currentScriptName] === 'undefined') ts[ts.currentScriptName] = {};
 ts[ts.currentScriptName].function = function(){
     const state = ts[ts.currentScriptName];
-  js player.isAdmin = true
-
+	var username = args[0];
+    admin add username;
 };
 ts[ts.currentScriptName].function();
