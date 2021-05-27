f4590b5391f4781b21d0e05e07e25908e033cdac
USMP-lancer <42185819+USMP-lancer@users.noreply.github.com>
2021-05-27 00:38:14 -0500

    updates the pirate kick with the translations from basedusers pirate brazillafier

--- global.js
+++ global.js
@@ -116,9 +116,7 @@ groundzero = ""
 kickpirated = function(p) {
     if (["VALVE", "IGGAMES", "Igruha.org", "tuttop", "CODEX", "FreeTP.Org"].includes(Strings.stripColors(p.name).trim())) {
         Call.kick(p.con, ""
-             + "Mindustry is free on [royal]https://anuke.itch.io/mindustry[]\n"
-             + "\n"
-             + "Mindustry можно скачать бесплатно на [royal]https://anuke.itch.io/mindustry[]\n"
+             + "Mindustry is free on [royal]https://anuke.itch.io/mindustry[]\n\nMindustry можно скачать бесплатно на [royal]https://anuke.itch.io/mindustry[]\n\n[#0000000][#fac11b80][#fac11b][][]\n[white][#fac11b][#fac11b80][][][]\n[white][#0][][#fac11b][][#0][][][#fac11b][#fac11b80][][]\n[#0][white][#fac11b][][][#fac11b][][]\n[#0][white][#fac11b][][][#fac11b][][][#fac11b80][]\n[#fac11b][#0][white][][white][][white][][]\n[#fac11b80][][#0][][white][]\n[#0][]\n\n\n\n[#fac11b80][][#fac11b80][][]\n[#fac11b80][#fac11b][][]\n[#fac11b80][#fac11b][][]"
              + "\n"
              + groundzero
         )
