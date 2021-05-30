fc0f67f79e02551575718792271d99ab7eb5efdc
buthed010203 <naguiar010203@gmail.com>
2021-05-30 15:29:02 -0400

    work

--- removewalls.js
+++ removewalls.js
@@ -7,7 +7,7 @@ function removeWalls(start){
         }
 
         let t = Vars.world.tile(i)
-        if (t.blockt.block() instanceof StaticWall) t.setNet(Blocks.air)
+        if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
     }
     // if ((start + 50) < (Vars.world.height() * Vars.world.width())) Core.app.post(() => removeWalls(start + 50))
 }
