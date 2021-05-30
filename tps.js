90f54849184d7dc6dab07e6b0115a6daeb77f323
buthed010203 <naguiar010203@gmail.com>
2021-05-30 11:17:49 -0400

    why am i even bothering making this pr

--- tps.js
+++ tps.js
@@ -1,46 +1,24 @@
 (function (){
-    const args = parseArguments(argument)
-
-    if (!ts.tps) ts.tps = { eventsRegistered: false, displays: {} }
-    const state = ts.tps
-
-    if (!state.eventsRegistered) {
+    if (!ts.tps) ts.tps = { eventsRegistered: false, players: {} }
+    if (!ts.tps.eventsRegistered) {
+        ts.tps.eventsRegistered = true
         const timer = new Interval()
+
         Events.run(Trigger.update, () => {
-            if (timer.get(60)) { // Run once a second
-                const tps = Core.graphics.getFramesPerSecond()
-                Groups.player.each(cons(p => {
-                    if (state.displays[p.uuid()] && state.displays[p.uuid()].enabled) {
-                        Call.infoPopup(p.con, 'TPS: ' + tps, 1,  Align.top, state.displays[p.uuid()].offset, 0, 0, 0) // 3 times to approximately match the background color of the item hud
-                        Call.infoPopup(p.con, 'TPS: ' + tps, 1,  Align.top, state.displays[p.uuid()].offset, 0, 0, 0)
-                        Call.infoPopup(p.con, 'TPS: ' + tps, 1,  Align.top, state.displays[p.uuid()].offset, 0, 0, 0)
-                    }
-                }))
+            if (timer.get(60)) { // 1s
+                for (const [player, padding] of Object.entries(ts.tps.players)) {
+                    Call.infoPopup(player.con, '[royal]Server TPS: []' + Core.graphics.getFramesPerSecond(), 1, Align.top, padding, 0, 0, 0)
+                }
             }
         })
-
-        state.eventsRegistered = true
     }
 
-    if (args.length > 0 && args[0][0] === 'd') {
-        const offset = args.length > 1 ? args[1] : 0
-
-        // running for the first time
-        if (!state.displays[me.uuid()]) {
-            state.displays[me.uuid()] = { enabled: true, offset: offset }
-            return "[royal]Enabled TPS display" + (offset > 0 ? " with offset " + offset : "")
-        }
-
-        // offset change
-        if (state.displays[me.uuid()].offset !== offset && args.length > 1) {
-            state.displays[me.uuid()] = { enabled: true, offset: offset }
-            return "[royal]Changed display offset to " + offset
-        }
-
-        // toggle
-        state.displays[me.uuid()].enabled = !state.displays[me.uuid()].enabled
-        return "[royal]" + (state.displays[me.uuid()].enabled ? "Enabled" : "Disabled") + " TPS display"
+    const args = parseArguments(argument)
+    if (args.length == 0) {
+        if (ts.tps.players.hasOwnProperty(me)) delete ts.tps.players[me]
+        return '[royal]Server TPS: []' + Core.graphics.getFramesPerSecond()
+    } else {
+        ts.tps.players[me] = args[0]
+        return '[royal]set TPS offset to ' + args[0]
     }
-
-    return '[royal]Server TPS: []' + Core.graphics.getFramesPerSecond()
 })();
