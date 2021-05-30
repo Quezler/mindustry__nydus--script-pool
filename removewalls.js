b31f938e392b31ea387f63e0f171e7420f9f7f51
buthed010203 <naguiar010203@gmail.com>
2021-05-30 13:45:57 -0400

    this is terrible but works

--- removewalls.js
+++ removewalls.js
@@ -1,14 +1,15 @@
+// Horrible and slow but it somewhat works kind of barely
 function removeWalls(){
     let i = 0
     Vars.world.tiles.eachTile(cons(t => {
         if (t.block() instanceof StaticWall) {
-            if (i++ == 100) {
-                Core.app.post(() => removeWalls()) // Do 100 per tick
-            } else if (i <= 100) {
+            if (i++ == 30) {
+                Core.app.post(() => removeWalls()) // Remove 30 walls a tick
+            } else if (i <= 30) {
                 t.setNet(Blocks.air)
             }
         }
     }))
-    if (i < 100) Groups.player.each(cons(p => sync(p)))
+    if (i < 30) Groups.player.each(cons(p => sync(p)))
 }
 removeWalls()
