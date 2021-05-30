1a2c524f98d1afc5c06908090ad47e78154e96bf
buthed010203 <naguiar010203@gmail.com>
2021-05-30 14:11:53 -0400

    those methods dont exist

--- removewalls.js
+++ removewalls.js
@@ -1,14 +1,14 @@
 // Horrible and slow but it somewhat works kind of barely
 function removeWalls(start){
     for (let i = start; i < start + 50; i++) {
-        if (i > Vars.world.tiles.size()) {
+        if (i > Vars.world.height * Vars.world.width) {
             Groups.player.each(cons(p => sync(p)))
             break
         }
 
-        let t = Vars.world.tiles.geti(i)
+        let t = Vars.world.tile(i)
         if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
     }
-    if (start + 50 < Vars.world.tiles.size()) Core.app.post(() => removeWalls(start + 50))
+    if (start + 50 < Vars.world.height * Vars.world.width) Core.app.post(() => removeWalls(start + 50))
 }
 removeWalls(0)
