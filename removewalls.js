9266ed120eb5ded89cd44c19f7943ec2049746a8
buthed010203 <naguiar010203@gmail.com>
2021-05-30 14:21:49 -0400

    real last commit for real not at all a lie

--- removewalls.js
+++ removewalls.js
@@ -1,7 +1,7 @@
 // Horrible and slow but it somewhat works kind of barely
 function removeWalls(start){
     for (let i = start; i < start + 50; i++) {
-        if (i > Vars.world.height * Vars.world.width) {
+        if (i > Vars.world.height() * Vars.world.width()) {
             Groups.player.each(cons(p => sync(p)))
             break
         }
@@ -9,6 +9,6 @@ function removeWalls(start){
         let t = Vars.world.tile(i)
         if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
     }
-    if (start + 50 < Vars.world.height * Vars.world.width) Core.app.post(() => removeWalls(start + 50))
+    if (start + 50 < Vars.world.height() * Vars.world.width()) Core.app.post(() => removeWalls(start + 50))
 }
 removeWalls(0)
