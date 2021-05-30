68f41d86698564acecea2ca04d307f551615e2c7
buthed010203 <naguiar010203@gmail.com>
2021-05-30 16:18:47 -0400

    saved?

--- removewalls.js
+++ removewalls.js
@@ -1,14 +1,16 @@
 // Horrible and slow but it somewhat works kind of barely
 function removeWalls(start){
-    for (let i = start; i < start + 3; i++) {
-        if (i > Vars.world.height() * Vars.world.width()) {
-            Groups.player.each(cons(p => sync(p)))
-            break
-        }
+    Core.app.post(() => {
+        for (let i = start; i < start + 3; i++) {
+            if (i > Vars.world.height() * Vars.world.width()) {
+                Groups.player.each(cons(p => sync(p)))
+                break
+            }
 
-        let t = Vars.world.tiles.geti(i)
-        if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
-    }
-    if (start + 3 < Vars.world.height() * Vars.world.width()) Core.app.post(() => Core.app.post(() => Core.app.post(removeWalls(start + 3))))
+            let t = Vars.world.tiles.geti(i)
+            if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
+        }
+        if (start + 3 < Vars.world.height() * Vars.world.width()) Core.app.post(removeWalls(start + 3))
+    })
 }
 removeWalls(0)
