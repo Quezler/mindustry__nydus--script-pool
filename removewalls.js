7ed80139ee13a2970897606950cdbf0b31373829
buthed010203 <naguiar010203@gmail.com>
2021-05-30 13:33:50 -0400

    ok i guess js is not a fan of arc?

--- removewalls.js
+++ removewalls.js
@@ -1,12 +1,13 @@
 removeWalls = (function (){
     let i = 0
     Vars.world.tiles.eachTile(cons(t => {
-        if (!t instanceof StaticWall) break
-        if (i++ >= 100) {
-            Core.app.post(() => removeWalls()) // Do 100 per tick
-            break
+        if (t instanceof StaticWall) {
+            if (i++ == 100) {
+                Core.app.post(() => removeWalls()) // Do 100 per tick
+            } else if (i <= 100) {
+                t.setNet(Blocks.air)
+            }
         }
-        t.setNet(Blocks.air)
     }))
     if (i < 100) Groups.player.each(cons(p => sync(p)))
 })();
