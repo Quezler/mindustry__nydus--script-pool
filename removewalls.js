a1fc1c1a94d419a8f8fa25bf25c3a09d5b6cf18f
buthed010203 <naguiar010203@gmail.com>
2021-05-30 13:31:39 -0400

    does break work?

--- removewalls.js
+++ removewalls.js
@@ -1,7 +1,7 @@
 removeWalls = (function (){
     let i = 0
     Vars.world.tiles.eachTile(cons(t => {
-        if (!t instanceof StaticWall) continue
+        if (!t instanceof StaticWall) break
         if (i++ >= 100) {
             Core.app.post(() => removeWalls()) // Do 100 per tick
             break
