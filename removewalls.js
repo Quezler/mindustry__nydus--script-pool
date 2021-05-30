f6e93147f7f5f7cefa26a001de9b566ecae704a9
buthed010203 <naguiar010203@gmail.com>
2021-05-30 14:55:01 -0400

    this time its sure to work!

--- removewalls.js
+++ removewalls.js
@@ -1,7 +1,6 @@
 // Horrible and slow but it somewhat works kind of barely
 (function removeWalls(){
     if (!ts.removeWalls) ts.removeWalls = 0
-    
     for (let i = ts.removeWalls; i < ts.removeWalls + 50; i++) {
         if (i > Vars.world.height * Vars.world.width) {
             Groups.player.each(cons(p => sync(p)))
@@ -11,6 +10,6 @@
         let t = Vars.world.tile(i)
         if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
     }
-    if (ts.removeWalls + 50 < Vars.world.height * Vars.world.width) Core.app.post(() => removeWalls())
     ts.removeWalls += 50
+    if (ts.removeWalls < Vars.world.height * Vars.world.width) Core.app.post(() => removeWalls())
 })();
