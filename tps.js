e52cf9099572c0e103f7febde3f85e2436e7343a
buthed010203 <naguiar010203@gmail.com>
2021-05-30 10:47:20 -0400

    Update tps.js

--- tps.js
+++ tps.js
@@ -1,17 +1,3 @@
 (function (){
-    if (!ts.tps) ts.tps = { eventsRegistered: false }
-    if (!ts.tps.eventsRegistered) {
-        const timer = new Interval()
-        var lastTick = 0
-        ts.mean = new WindowedMean(60)
-
-        Events.run(Trigger.update, () => {
-            ts.mean.add(Time.timeSinceMillis(lastTick))
-            lastTick = Time.millis()
-        })
-
-        ts.tps.eventsRegistered = true
-    }
-
-    return '[royal]Server TPS: []' + Math.min(60, Mathf.round(1000 / ts.mean.rawMean()))
+    return '[royal]Server TPS: []' + Core.graphics.getFramesPerSecond()
 })();
