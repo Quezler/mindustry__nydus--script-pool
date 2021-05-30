d60fe26dcbdfafcaec419397efbdd87d8bbcde9c
buthed010203 <naguiar010203@gmail.com>
2021-05-30 13:29:57 -0400

    fix removewalls

--- removewalls.js
+++ removewalls.js
@@ -1,11 +1,12 @@
 removeWalls = (function (){
     let i = 0
-    Vars.world.tiles.eachTile(t => {
+    Vars.world.tiles.eachTile(cons(t => {
         if (!t instanceof StaticWall) continue
         if (i++ >= 100) {
             Core.app.post(() => removeWalls()) // Do 100 per tick
             break
         }
         t.setNet(Blocks.air)
-    })
+    }))
+    if (i < 100) Groups.player.each(cons(p => sync(p)))
 })();
