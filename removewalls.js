fc6c771295ea2d71a7a013808dd15800e119a3b5
buthed010203 <naguiar010203@gmail.com>
2021-05-30 14:07:52 -0400

    These arent static :groundzero:

--- removewalls.js
+++ removewalls.js
@@ -1,14 +1,14 @@
 // Horrible and slow but it somewhat works kind of barely
 function removeWalls(start){
     for (let i = start; i < start + 50; i++) {
-        if (i > Tiles.size()) {
+        if (i > Vars.world.tiles.size()) {
             Groups.player.each(cons(p => sync(p)))
             break
         }
 
-        let t = Tiles.geti(i)
+        let t = Vars.world.tiles.geti(i)
         if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
     }
-    if (start + 50 < Tiles.size()) Core.app.post(() => removeWalls(start + 50))
+    if (start + 50 < Vars.world.tiles.size()) Core.app.post(() => removeWalls(start + 50))
 }
 removeWalls(0)
