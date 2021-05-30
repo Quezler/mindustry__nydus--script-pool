00b0645b9a501c9714b576092ad7e30986734464
buthed010203 <naguiar010203@gmail.com>
2021-05-30 17:02:46 -0400

    suddenly broken?

--- removewalls.js
+++ removewalls.js
@@ -1,19 +1,21 @@
 // Horrible and slow but it somewhat works kind of barely
-if (!ts.removeWalls) {
-    Events.run(Trigger.update, () => {
-        if (ts.removeWalls == -1) return
-        const start = Time.millis()
-        const tiles = Vars.world.height() * Vars.world.width()
-        while (Time.timeSinceMillis(start < 33.3)) {
-            if (ts.removeWalls > tiles) {
-                ts.removeWalls = -1
-                Groups.player.each(cons(p => sync(p)))
-                return
-            }
+(function(){
+    if (!ts.removeWalls) {
+        Events.run(Trigger.update, () => {
+            if (ts.removeWalls == -1) return
+            const start = Time.millis()
+            const tiles = Vars.world.height() * Vars.world.width()
+            while (Time.timeSinceMillis(start < 33.3)) {
+                if (ts.removeWalls > tiles) {
+                    ts.removeWalls = -1
+                    Groups.player.each(cons(p => sync(p)))
+                    return
+                }
 
-            const t = Vars.world.tiles.geti(ts.removeWalls++)
-            if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
-        }
-    })
-}
-ts.removeWalls = 0
+                const t = Vars.world.tiles.geti(ts.removeWalls++)
+                if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
+            }
+        })
+    }
+    ts.removeWalls = 0
+})();
