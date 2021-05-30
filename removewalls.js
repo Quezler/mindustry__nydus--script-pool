6ffa02dfb1b246b4c8ae6a2500ced53b0d652a4a
buthed010203 <naguiar010203@gmail.com>
2021-05-30 13:36:54 -0400

    ok it works for real

--- removewalls.js
+++ removewalls.js
@@ -1,7 +1,7 @@
 removeWalls = (function (){
     let i = 0
     Vars.world.tiles.eachTile(cons(t => {
-        if (t instanceof StaticWall) {
+        if (t.block() instanceof StaticWall) {
             if (i++ == 100) {
                 Core.app.post(() => removeWalls()) // Do 100 per tick
             } else if (i <= 100) {
