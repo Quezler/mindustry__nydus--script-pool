ca4293144d608e8701835fad87e307e25646319a
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2021-05-31 09:29:38 +0500

    Update removewalls.js

--- removewalls.js
+++ removewalls.js
@@ -1,21 +1,9 @@
 // Horrible and slow but it somewhat works kind of barely
 (function(){
-    if (!ts.removeWalls) {
-        Events.run(Trigger.update, () => {
-            if (ts.removeWalls == -1) return
-            const start = Time.millis()
-            const tiles = Vars.world.height() * Vars.world.width()
-            while (Time.timeSinceMillis(start) < 33.33) { // Run for 33.3 ms every tick
-                if (ts.removeWalls >= tiles) {
-                    ts.removeWalls = -1
-                    Groups.player.each(cons(p => sync(p)))
-                    return
-                }
-
-                const t = Vars.world.tiles.geti(ts.removeWalls++)
-                if (t.block() instanceof StaticWall) t.setBlock(Blocks.air) // setNet created unneeded packet spam as we sync at the end either way.
-            }
+    Timer.schedule(() => {
+        Vars.world.tiles.eachTile(tile => {
+            if (t.block() instanceof StaticWall) t.setBlock(Blocks.air)
         })
-    }
-    ts.removeWalls = 0
+        Groups.player.each(p => sync(p))
+    }, 0.5)
 })();
