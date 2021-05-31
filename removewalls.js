4a5e47b619ee1456d5ccfac924a71086d3c4302d
buthed010203 <naguiar010203@gmail.com>
2021-05-30 23:11:24 -0400

    optimized

--- removewalls.js
+++ removewalls.js
@@ -5,7 +5,7 @@
             if (ts.removeWalls == -1) return
             const start = Time.millis()
             const tiles = Vars.world.height() * Vars.world.width()
-            while (Time.timeSinceMillis(start) < 33.33) { // Will drop the tps down to 30 until finished
+            while (Time.timeSinceMillis(start) < 33.33) { // Run for 33.3 ms every tick
                 if (ts.removeWalls >= tiles) {
                     ts.removeWalls = -1
                     Groups.player.each(cons(p => sync(p)))
@@ -13,7 +13,7 @@
                 }
 
                 const t = Vars.world.tiles.geti(ts.removeWalls++)
-                if (t.block() instanceof StaticWall) t.setNet(Blocks.air)
+                if (t.block() instanceof StaticWall) t.setBlock(Blocks.air) // setNet created unneeded packet spam as we sync at the end either way.
             }
         })
     }
