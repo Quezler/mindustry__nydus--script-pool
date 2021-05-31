0f6d4b5b7850c3900a2203fb1870c11b3ec50ce0
Blockzilla101 <48705529+Blockzilla101@users.noreply.github.com>
2021-05-31 09:34:10 +0500

    .

--- removewalls.js
+++ removewalls.js
@@ -1,7 +1,7 @@
 // Horrible and slow but it somewhat works kind of barely
 (function(){
     Timer.schedule(() => {
-        Vars.world.tiles.eachTile(tile => {
+        Vars.world.tiles.eachTile(t => {
             if (t.block() instanceof StaticWall) t.setBlock(Blocks.air)
         })
         Groups.player.each(p => sync(p))
